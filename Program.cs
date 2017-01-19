ddb7930e7463e23344212eba7eaf810f204a2068
mcSHLANG <anpilogov.a.v@gmail.com>
2017-01-19 17:09:12 +0400

    HOTFIX2

--- Program.cs
+++ Program.cs
@@ -10,20 +10,7 @@ namespace TestCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hotfix one");
             Console.WriteLine("hotfix two");
-            Console.WriteLine("two hotfix");
-            Console.WriteLine("three hotfix");
-            Console.WriteLine("Four hotfix");
-            Console.WriteLine("hotfix test two");
-            Console.WriteLine("Test one hotfix");
-
-            Console.WriteLine("hotfix-one");
-            Console.WriteLine("hotfix-one_one");
-            
-            Console.WriteLine("hotfix-two");
-            Console.WriteLine("hotfix-two_two");
-            Console.WriteLine("hotfix-two");
         }
     }
 }
