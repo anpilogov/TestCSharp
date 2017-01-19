b8e76e939f27bfd794931a413d6de2b2bccda2ae
mcSHLANG <anpilogov.a.v@gmail.com>
2017-01-19 17:02:00 +0400

    aaa

--- Program.cs
+++ Program.cs
@@ -18,15 +18,7 @@ namespace TestCSharp
             Console.WriteLine("hotfix test two");
             Console.WriteLine("Test one hotfix");
 
-<<<<<<< 37c1c413c21fbe94fe12eb62862450cb23f8cfe6
             Console.WriteLine("hotfix-one");
-            Console.WriteLine("hotfix-one_one");
-            
-            Console.WriteLine("hotfix-two");
-            Console.WriteLine("hotfix-two_two");
-=======
-            Console.WriteLine("hotfix-two");
->>>>>>> b316e49ec257b2ab516945a6e0eabf936ff1d457
         }
     }
 }
