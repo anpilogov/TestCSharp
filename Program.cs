f76f39b55be6c0b759056a799cb651bca010278e
Anpilogov Anton <anpilogov.a.v@gmail.com>
2017-01-23 11:46:31 +0400

    Изменение в ветке development

--- Program.cs
+++ Program.cs
@@ -29,6 +29,8 @@ namespace TestCSharp
             Console.WriteLine("hotfix 1.1.2");
 
             Console.WriteLine("Editing for development branch two");
+
+            Console.WriteLine("Editing for development branch three");
         }
     }
 }
